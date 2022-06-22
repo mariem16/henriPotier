ba23195e63f7711e04a3461823bf70eb5520c85b
mbenrejeb <Moulasession@2>
2022-06-22 17:35:19 +0100

    fix crash when offline

--- app/src/main/java/com/test/henripotier/repository/BookRepository.kt
+++ app/src/main/java/com/test/henripotier/repository/BookRepository.kt
@@ -1,26 +1,34 @@
 package com.test.henripotier.repository
 
 import androidx.paging.PagingSource
+import com.test.henripotier.api.ApiService
 import com.test.henripotier.data.dao.BookDao
 import com.test.henripotier.data.model.Book
-import com.test.henripotier.api.ApiService
 import com.test.henripotier.utils.Response
+import java.net.UnknownHostException
 
-class BookRepository(private val apiService: ApiService, private val bookDao : BookDao) {
+class BookRepository(private val apiService: ApiService, private val bookDao: BookDao) {
 
     suspend fun getBooksRemotely(): Response<Nothing> {
-        val result = apiService.getBooks()
-        return if(result.isSuccessful){
-            val books = result.body() as List<Book>
-            bookDao.insertAll(books)
-            Response.Success()
-        }else {
-            Response.Error(result.errorBody().toString())
+        return try {
+            val result = apiService.getBooks()
+            if (result.isSuccessful) {
+                val books = result.body() as List<Book>
+                bookDao.insertAll(books)
+                Response.Success()
+            } else {
+                Response.Error(result.errorBody().toString())
+            }
+        } catch (exception : UnknownHostException) {
+            Response.Error("No internet connection!")
+        } catch (exception: Exception) {
+            Response.Error("error occurs when trying to fetch data!")
         }
+
     }
 
     fun getLocalBooks(): PagingSource<Int, Book> {
-         return bookDao.getPagedBook()
+        return bookDao.getPagedBook()
     }
 
     suspend fun getBooksByIsbn(isbns: List<String>) = bookDao.getBooksByIsbn(isbns)
--- app/src/main/java/com/test/henripotier/repository/CartRepository.kt
+++ app/src/main/java/com/test/henripotier/repository/CartRepository.kt
@@ -1,19 +1,27 @@
 package com.test.henripotier.repository
 
-import com.test.henripotier.data.model.CommercialOffers
 import com.test.henripotier.api.ApiService
+import com.test.henripotier.data.model.CommercialOffers
 import com.test.henripotier.utils.Response
+import java.net.UnknownHostException
 
 class CartRepository(private val apiService: ApiService) {
 
-    suspend fun getOffers(isbns : List<String>?) : Response<CommercialOffers> {
-        val result = apiService.getOffers(isbns?.joinToString(","))
-        return if(result.isSuccessful){
-            val books = result.body() as CommercialOffers
-             Response.Success(books)
-        }else {
-            Response.Error(result.errorBody().toString())
+    suspend fun getOffers(isbns: List<String>?): Response<CommercialOffers> {
+        return try {
+            val result = apiService.getOffers(isbns?.joinToString(","))
+            if (result.isSuccessful) {
+                val books = result.body() as CommercialOffers
+                Response.Success(books)
+            } else {
+                Response.Error(result.errorBody().toString())
+            }
+        } catch (exception : UnknownHostException) {
+            Response.Error("No internet connection!")
+        } catch (exception: Exception) {
+            Response.Error("error occurs when trying to fetch data!")
         }
+
     }
 
 }
